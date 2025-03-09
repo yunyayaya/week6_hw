3eda860ce3f4fa3d93b18e43d79150e1a766428b
Yunya <alice504119@gmail.com>
2025-03-09 19:29:57 +0800

    update json

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  bbase: process.env.Node_ENV === 'production' ? '/week6_hw/' : '/',
+  base: process.env.Node_ENV === 'production' ? '/week6_hw/' : '/',
   plugins: [react()],
 });
